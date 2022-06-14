a49fd2305aee94afc9f82a9322f7e896155f41a9
DanielMalede <danielmalede@gmail.com>
2022-06-14 23:45:05 +0300

    .

--- main.js
+++ main.js
@@ -14,10 +14,23 @@
 // }
 // orderArrayNumbers([55,1,7,9,8,6,3,44,51,15,4,8,1])
 
-var usernames = document.getElementById("userNames")
-var myArray = []
-function createStudentNames() {
-        myArray.push(usernames.value)
+// let usernames = document.getElementById("userNames")
+// let myArray = []
+// function createStudentNames() {
+//     if (myArray.length<10) {
+//         myArray.push(usernames.value)
+//         usernames.value = ""
+//     }
+//     console.log(myArray.join("-")); 
+// }
 
-    }
-console.log(myArray);
+// let numbersArray = []
+// function createRandomArrayAges() {
+//     for (let i = 0; i < 10; i++) {
+//             numbersArray.push(Math.floor(Math.random()*121))
+//     }
+//     console.log(numbersArray.sort((a,b)=>a-b));
+// }
+// createRandomArrayAges()
+
+
